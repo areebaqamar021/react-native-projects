7f50b618350081deb578f31da79e3e0558dd3652
areebaqamar021 <areebaqamar021@gmail.com>
2024-08-24 23:29:33 +0500

    feat: Add MongooseModule and PharmacyModule imports to app.module.ts

--- CRUD functions/pharmacy-backend/src/app.module.ts
+++ CRUD functions/pharmacy-backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PharmacyModule } from './pharmacy/pharmacy.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('YOUR_MONGODB_ATLAS_CONNECTION_STRING'),
+    PharmacyModule,
+  ],
 })
 export class AppModule {}
