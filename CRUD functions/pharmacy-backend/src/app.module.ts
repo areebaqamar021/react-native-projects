c7187efd4619dd6bd48a7ea49d43f53aeb0adbc0
areebaqamar021 <areebaqamar021@gmail.com>
2024-08-27 14:40:46 +0500

    chore: Update MongoDB connection string in app.module.ts

--- CRUD functions/pharmacy-backend/src/app.module.ts
+++ CRUD functions/pharmacy-backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { PharmacyModule } from './pharmacy/pharmacy.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     PharmacyModule,
   ],
