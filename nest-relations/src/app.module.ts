e0be41fa42c35affd5416097ecb835733a1ab8f9
areebaqamar021 <areebaqamar021@gmail.com>
2024-09-09 12:29:52 +0500

    upadte some modules and clean the code

--- nest-relations/src/app.module.ts
+++ nest-relations/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+ 
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '', 
+      database: 'pharmacy_db', 
+      synchronize: true,// Indicates if database schema should be auto created on every start
+      autoLoadEntities: true,
+      //Logger setings to log error's and warn's in the ORM.      
+      logger:'file',
+      logging:["error"]
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
