3bbff908d30faa48a9266fede37493381de2bb96
areebaqamar021 <areebaqamar021@gmail.com>
2024-09-11 11:36:33 +0500

    created modules for student,course and enrollment

--- nest-relations/src/enrollment/enrollment.module.ts
+++ nest-relations/src/enrollment/enrollment.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { EnrollmentController } from './enrollment.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Enrollment } from './enrollment.entity';
 import { EnrollmentService } from './enrollment.service';
+import { EnrollmentController } from './enrollment.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Enrollment])],
+  providers: [EnrollmentService],
   controllers: [EnrollmentController],
-  providers: [EnrollmentService]
 })
 export class EnrollmentModule {}
