e7a0d900bd9c3e669103770f45ea9d37c62149ea
areebaqamar021 <areebaqamar021@gmail.com>
2024-09-11 12:38:01 +0500

    remove all the files and making it from start

--- nest-relations/src/app.module.ts
+++ nest-relations/src/app.module.ts
@@ -1,14 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { UserProfileModule } from './user-profile/user-profile.module';
-import { CategoryController } from './category/category.controller';
-import { CategoryModule } from './category/category.module';
-import { ProductService } from './product/product.service';
-import { ProductModule } from './product/product.module';
-import { StudentModule } from './student/student.module';
-import { CourseModule } from './course/course.module';
-import { EnrollmentModule } from './enrollment/enrollment.module';
 
 @Module({
   imports: [
@@ -24,16 +15,6 @@ import { EnrollmentModule } from './enrollment/enrollment.module';
       ],
       synchronize: true, // Set to false in production
     }),
-    UserModule,
-    UserProfileModule,
-    CategoryModule,
-    ProductModule,
-    StudentModule,
-    CourseModule,
-    EnrollmentModule,
   ],
-  controllers: [CategoryController],
-  
-  providers: [ProductService],
 })
 export class AppModule {}
