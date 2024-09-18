e7bdf5f839a67f575ddac33b27179ed2f7ecd961
areebaqamar021 <areebaqamar021@gmail.com>
2024-09-18 18:54:07 +0500

    done with changes in calender screen

--- practiceApp/App.tsx
+++ practiceApp/App.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { View, Button, Text } from 'react-native';
 import TimerApp from './src/components/TimerApp';
 import CalendarComponent from './src/components/myCalender';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({ navigation }: { navigation: any }) => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <TimerApp />
-      <CalendarComponent />
-    </SafeAreaView>
+    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24, marginBottom: 20 }}>Main Menu</Text>
+      <Button title="Open Calendar" onPress={() => navigation.navigate('Calendar')} />
+      <Button title="Open Timer" onPress={() => navigation.navigate('Timer')} />
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-  },
-});
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Calendar" component={CalendarComponent} />
+        <Stack.Screen name="Timer" component={TimerApp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
--- practiceApp/src/components/myCalender.js
+++ practiceApp/src/components/myCalender.js
@@ -6,7 +6,23 @@ const CalendarComponent = () => {
   return (
     <View style={{ flex: 1, padding: 20 }}>
       <Text style={{ fontSize: 24, marginBottom: 20 }}>Calendar</Text>
-      <Calendar />
+      <Calendar
+       style={{
+        borderWidth: 1,
+        borderColor: 'gray',
+        height: 350,
+      }}
+      theme={{
+        backgroundColor: '#ffffff',
+        calendarBackground: '#ffffff',
+        textSectionTitleColor: '#b6c1cd',
+        selectedDayBackgroundColor: '#00adf5',
+        selectedDayTextColor: '#ffffff',
+        todayTextColor: '#00adf5',
+        dayTextColor: '#2d4150',
+        textDisabledColor: '#dd99ee'
+      }}
+      />
     </View>
   );
 };
