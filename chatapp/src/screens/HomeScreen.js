b01d6569cfd249a81cf6bcb2d06198a06e495c0a
areebaqamar021 <areebaqamar021@gmail.com>
2024-10-18 22:35:55 +0500

    Refactor imports in HomeScreen.js and ChatRoom.js

--- chatapp/src/screens/HomeScreen.js
+++ chatapp/src/screens/HomeScreen.js
@@ -1,6 +1,5 @@
-import { Text, StyleSheet, TextInput, View, TouchableOpacity, FlatList } from 'react-native'
+import { StyleSheet, TextInput, View, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react';
-import firestore from '@react-native-firebase/firestore';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Logout from './Logout';
@@ -9,28 +8,6 @@ import auth from '@react-native-firebase/auth';
 const HomeScreen = () => {
     const navigation = useNavigation();
     const [currentUserId, setCurrentUserId] = useState(null);
-    const [chatUsers, setChatUsers] = useState([]);
-
-    useEffect(() => {
-        const fetchChatUsers = async () => {
-            const currentUser = auth().currentUser;
-
-            const chatsSnapshot = await firestore()
-                .collection('chats')
-                .where('users', 'array-contains', currentUser.uid)
-                .get();
-
-            const users = chatsSnapshot.docs.map(doc => {
-                const chatData = doc.data();
-                const otherUser = chatData.users.find(userId => userId !== currentUser.uid);
-                return { id: doc.id, otherUser };
-            });
-
-            setChatUsers(users);
-        };
-
-        fetchChatUsers();
-    }, []);
 
     useEffect(() => {
         const user = auth().currentUser;
@@ -41,31 +18,13 @@ const HomeScreen = () => {
         }
     }, []);
 
-    const goToChatRoom = (chatId, otherUserId) => {
-        navigation.navigate('ChatRoom', { chatId, otherUserId });
-    };
-
     return (
         <View style={styles.container}>
             <TextInput
                 style={styles.searchBar}
                 placeholder="Search Users..."
             />
-            {/* <FlatList
-                data={chatUsers}
-                keyExtractor={item => item.id}
-                renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => goToChatRoom(item.id, item.otherUser)}>
-                        <Text>Chat with {item.otherUser}</Text>
-                    </TouchableOpacity>
-                )}
-            /> */}
             <Logout />
-            {/* {currentUserId ? (
-                <ChatRoomList userId={currentUserId} navigation={props.navigation} />
-            ) : (
-                <Text>User not logged in</Text>
-            )} */}
             <TouchableOpacity style={styles.addButton} onPress={() => navigation.navigate('Users', { currentUserId })}>
                 <Icon name="add-box" size={50} />
             </TouchableOpacity>
--- chatapp/src/services/firestoreService.js
+++ chatapp/src/services/firestoreService.js
@@ -9,125 +9,88 @@ export const getAllUsers = async () => {
     return usersSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 };
 
-// export const createChatRoom = async roomName => {
-//   try {
-//     const roomRef = await firestore()
-//       .collection('chatRooms')
-//       .add({name: roomName, createdAt: firestore.FieldValue.serverTimestamp()});
-//     return roomRef;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+const getUserNameById = async (userId) => {
+    const userDoc = await firestore().collection('Users').doc(userId).get(); 
+    console.log("userDoc", userDoc);
+    return userDoc.exists ? userDoc.data().name : null;
+};
 
-// export const getChatRooms = (setChatRooms) => {
-//   return firestore()
-//     .collection('chatRooms')
-//     .orderBy('createdAt', 'desc')
-//     .onSnapshot(snapshot => {
-//       const rooms = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-//       setChatRooms(rooms);
-//     });
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const getUserNameById = async (userId) => {
-//     const userDoc = await firestore().collection('Users').doc(userId).get(); 
-//     console.log("userDoc", userDoc);
-//     return userDoc.exists ? userDoc.data().name : null;
-// };
-
-// export const sendMessage = async (text, senderId, receiverId) => {
-//     const chatRoomId = await createOrGetChatRoom(senderId, receiverId);
+export const sendMessage = async (text, senderId, receiverId) => {
+    const chatRoomId = await createOrGetChatRoom(senderId, receiverId);
   
     // Add the message to the messages sub-collection
-    // const messageRef = firestore()
-    //   .collection('chatrooms')
-    //   .doc(chatRoomId)
-    //   .collection('messages');
+    const messageRef = firestore()
+      .collection('chatrooms')
+      .doc(chatRoomId)
+      .collection('messages');
   
-    // await messageRef.add({
-    //   text,
-    //   senderId,
-    //   createdAt: firestore.FieldValue.serverTimestamp(),
-    // });
+    await messageRef.add({
+      text,
+      senderId,
+      createdAt: firestore.FieldValue.serverTimestamp(),
+    });
   
     // Update the last message and timestamp in the chat room
-  //   await firestore()
-  //     .collection('chatrooms')
-  //     .doc(chatRoomId)
-  //     .update({
-  //       lastMessage: text,
-  //       updatedAt: firestore.FieldValue.serverTimestamp(),
-  //     });
-  // };
+    await firestore()
+      .collection('chatrooms')
+      .doc(chatRoomId)
+      .update({
+        lastMessage: text,
+        updatedAt: firestore.FieldValue.serverTimestamp(),
+      });
+  };
   
 
-//   export const getMessages = (senderId, receiverId, callback) => {
-//     const chatRoomId = [senderId, receiverId].sort().join('_'); 
+  export const getMessages = (senderId, receiverId, callback) => {
+    const chatRoomId = [senderId, receiverId].sort().join('_'); 
   
-//     const unsubscribe = firestore()
-//       .collection('chatrooms')
-//       .doc(chatRoomId)
-//       .collection('messages')
-//       .orderBy('createdAt', 'asc')
-//       .onSnapshot(snapshot => {
-//         const messages = snapshot.docs.map(doc => ({
-//           id: doc.id,
-//           ...doc.data(),
-//         }));
-//         callback(messages);
-//       });
+    const unsubscribe = firestore()
+      .collection('chatrooms')
+      .doc(chatRoomId)
+      .collection('messages')
+      .orderBy('createdAt', 'asc')
+      .onSnapshot(snapshot => {
+        const messages = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        callback(messages);
+      });
   
-//     return unsubscribe;
-//   };
+    return unsubscribe;
+  };
   
 
-// export const createOrGetChatRoom = async (senderId, receiverId) => {
-//   const chatRoomId = [senderId, receiverId].sort().join('_'); 
+export const createOrGetChatRoom = async (senderId, receiverId) => {
+  const chatRoomId = [senderId, receiverId].sort().join('_'); 
 
-//   const chatRoomRef = firestore().collection('chatrooms').doc(chatRoomId);
+  const chatRoomRef = firestore().collection('chatrooms').doc(chatRoomId);
 
-//   const chatRoomSnapshot = await chatRoomRef.get();
+  const chatRoomSnapshot = await chatRoomRef.get();
   
-//   if (!chatRoomSnapshot.exists) {
-//     await chatRoomRef.set({
-//       participants: [senderId, receiverId],
-//       createdAt: firestore.FieldValue.serverTimestamp(),
-//       updatedAt: firestore.FieldValue.serverTimestamp(),
-//       lastMessage: '',
-//     });
-//   }
+  if (!chatRoomSnapshot.exists) {
+    await chatRoomRef.set({
+      participants: [senderId, receiverId],
+      createdAt: firestore.FieldValue.serverTimestamp(),
+      updatedAt: firestore.FieldValue.serverTimestamp(),
+      lastMessage: '',
+    });
+  }
   
-//   return chatRoomId; 
-// };
+  return chatRoomId; 
+};
 
-// export const getChatRooms = async (userId) => {
-//     const chatRoomsSnapshot = await firestore()
-//       .collection('chatrooms')
-//       .where('messages', 'array-contains', userId)
-//       .orderBy('updatedAt', 'desc')
-//       .get();
+export const getChatRooms = async (userId) => {
+    const chatRoomsSnapshot = await firestore()
+      .collection('chatrooms')
+      .where('messages', 'array-contains', userId)
+      .orderBy('updatedAt', 'desc')
+      .get();
   
-//     return chatRoomsSnapshot.docs.map(doc => ({
-//       id: doc.id, 
-//       ...doc.data(), 
-//     }));
-//   };
+    return chatRoomsSnapshot.docs.map(doc => ({
+      id: doc.id, 
+      ...doc.data(), 
+    }));
+  };
   
 
