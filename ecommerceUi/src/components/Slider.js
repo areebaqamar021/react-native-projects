34cafbc511cfa98dbade7213563510609b14658d
areebaqamar021 <areebaqamar021@gmail.com>
2024-10-23 18:03:43 +0500

    adding some new files and packages

--- ecommerceUi/src/components/Slider.js
+++ ecommerceUi/src/components/Slider.js
@@ -1,14 +1,76 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+
+const { width: viewportWidth } = Dimensions.get('window');
+
+const images = [
+  require('../assets/Image.png'),
+];
 
 const Slider = () => {
+  const carouselRef = useRef(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const renderItem = ({ item }) => {
+    return (
+      <Image source={item} style={styles.image} resizeMode="cover" />
+    );
+  };
+
   return (
-    <View>
-      <Text>Slider</Text>
+    <View style={styles.container}>
+      <Carousel
+        layout="default"
+        ref={carouselRef}
+        data={images}
+        renderItem={renderItem}
+        sliderWidth={viewportWidth}
+        itemWidth={viewportWidth}
+        onSnapToItem={(index) => setActiveIndex(index)}
+      />
+      <View style={styles.indicatorContainer}>
+        {images.map((_, index) => (
+          <View
+            key={index}
+            style={[
+              styles.indicatorLine,
+              index === activeIndex ? styles.activeIndicator : styles.inactiveIndicator,
+            ]}
+          />
+        ))}
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Slider
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: viewportWidth,
+    height: 200,
+  },
+  indicatorContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  indicatorLine: {
+    width: 20,
+    height: 5,
+    marginHorizontal: 5,
+    borderRadius: 3,
+  },
+  activeIndicator: {
+    backgroundColor: 'blue',
+  },
+  inactiveIndicator: {
+    backgroundColor: 'lightgray',
+  },
+});
 
-const styles = StyleSheet.create({})
+export default Slider;
--- ecommerceUi/src/screens/Main.js
+++ ecommerceUi/src/screens/Main.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Header from '../components/Header'
+import Slider from '../components/Slider'
 
 const Main = () => {
   return (
     <View>
       <Header />
+      <Slider />
     </View>
   )
 }
