795090076494e16b12ab98f1823660658f9e5620
areebaqamar021 <areebaqamar021@gmail.com>
2024-10-26 21:37:52 +0500

    Refactor code: Add ProductList screen

--- ecommerceUi/src/screens/ProductList.js
+++ ecommerceUi/src/screens/ProductList.js
@@ -1,24 +1,17 @@
-import { FlatList, StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import ProductCard from '../components/ProductCard';
 import React from 'react'
-import Header from '../components/Header'
-import Slider from '../components/Slider'
-import ProductCard from '../components/ProductCard'
-import Categories from './Categories'
-import ProductList from './ProductList'
 
-const Main = () => {
-    return (
-        <ScrollView style={{ flex: 1 }} >
-            <Header />
-            <Slider />
-            <View style={styles.view}>
-                <Text style={styles.deals}>Hot Deals</Text>
+const ProductList = () => {
+  return (
+    <View>
+      <View style={styles.view}>
+                <Text style={styles.deals}>Product list</Text>
                 <TouchableOpacity>
                     <Text style={styles.more}> View more   > </Text>
                 </TouchableOpacity>
             </View>
-            <View>
-                <FlatList
+            <FlatList
                     contentContainerStyle={{ alignSelf: 'flex-start' }}
                     data={[
                         {
@@ -51,20 +44,29 @@ const Main = () => {
                             rating: 5,
                             reviewCount: 65
                         },
+                        {
+                            name: "product name 6",
+                            image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?q=80&w=2899&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                            rating: 5,
+                            reviewCount: 65
+                        },
                     ]}
                     renderItem={({ item, index }) => {
-                        return <ProductCard item = {item}/>
+                        return <ProductCard item = {item} imageStyles={{
+                                width: 175,
+                                height: 175,
+                                marginBottom: 7,
+                                backgroundColor: 'red'
+                        }}/>
                     }}
-                    horizontal={true}
+                    scrollEnabled={false}
+                    numColumns={2}
                 />
-                </View>
-            <Categories />
-            <ProductList />
-        </ScrollView>
-    )
+    </View>
+  )
 }
 
-export default Main
+export default ProductList
 
 const styles = StyleSheet.create({
     view: {
